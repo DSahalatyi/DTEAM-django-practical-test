b378fe1b28fad5856b271d076b6e916f1b81d2b7
DSahalatyi <dsahalatyi@gmail.com>
2025-05-29 13:17:26 +0300

    Add tests for CV download

--- main/tests/test_cv_views.py
+++ main/tests/test_cv_views.py
@@ -25,3 +25,20 @@ class CVDetailTests(BaseCVTests):
         response = self.client.get(self.cv.get_absolute_url())
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, "main/cv_detail.html")
+
+
+class CDDownloadTests(BaseCVTests):
+    CV_DOWNLOAD_URL = reverse("main:cv-download", kwargs={"pk": 1})
+
+    def test_pdf_download_returns_pdf(self):
+        response = self.client.get(self.CV_DOWNLOAD_URL)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response["Content-Type"], "application/pdf")
+
+    def test_pdf_download_has_correct_filename(self):
+        response = self.client.get(self.CV_DOWNLOAD_URL)
+        content_disposition = response["Content-Disposition"]
+        self.assertIn("attachment", content_disposition)
+        self.assertIn(
+            f"{self.cv.first_name}_{self.cv.last_name}_cv.pdf", content_disposition
+        )
