befad0b286582ed5e56d333e3c765a878df164bf
DSahalatyi <dsahalatyi@gmail.com>
2025-05-29 12:57:06 +0300

    Add CV download as PDF button, rearrange templates, update dependencies

--- main/urls.py
+++ main/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from main.views import CVListView, CVDetailView
+from main.views import CVListView, CVDetailView, CVDownloadPDFView
 
 app_name = "main"
 
 urlpatterns = [
     path("", CVListView.as_view(), name="cv-list"),
     path("cv/<int:pk>/", CVDetailView.as_view(), name="cv-detail"),
+    path("cv/<int:pk>/download/", CVDownloadPDFView.as_view(), name="cv-download"),
 ]
