3487ce5b627915a1d4530b02a1381a68a5992406
DSahalatyi <dsahalatyi@gmail.com>
2025-05-30 18:42:45 +0300

    Add more debug output for task creation

--- main/views.py
+++ main/views.py
@@ -120,7 +120,13 @@ class CVSendEmailView(View):
             return redirect(request.META.get("HTTP_REFERER", "/"))
 
         cv = get_object_or_404(CV, pk=pk)
-        print("Creating send mail task")
-        send_cv_email.delay(cv.id, email)
+
+        try:
+            print("Creating send mail task")
+            result = send_cv_email.delay(cv.id, email)
+            print(f"Task result - {result}")
+        except Exception as e:
+            print(e)
+
         messages.success(request, "CV is being sent to the specified email.")
         return redirect(request.META.get("HTTP_REFERER", "/"))
